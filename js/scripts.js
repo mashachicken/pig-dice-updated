76c85e546985bceb1ece0f55824f91725f049c0f
Student <example@example.com>
2019-11-07 11:23:50 -0800

    everything works! for now...

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,62 @@
+//
+// Game.prototype.addScores = function(turn) {
+//   for (var i = 1; i < turn.length; i++){
+//     if (turn.includes(1)){
+//       alert("Next player's turn")
+//     } else if  (turn.includes(2) || turn.includes(3) || turn.includes(4) || turn.includes(5) || turn.includes(6))  {
+//       console.log('hit the else if');
+//     }
+//     console.log("working")
+//   }
+// return turn
+// }
+// // }
+//
 
 
-Game.prototype.addScores = function(turn) {
-  for (var i = 1; i < turn.length; i++){
-    if (turn.includes(1)){
-      alert("Next player's turn")
-    } else if  (turn.includes(2) || turn.includes(3) || turn.includes(4) || turn.includes(5) || turn.includes(6))  {
-      console.log('hit the else if');
-    }
-    console.log("working")
-  }
-return turn
+//
+//
+// Player1.prototype.addScore
+//
+
+function Player () {
+  this.score = [];
+  this.turn = []
 }
+
+
+Player.prototype.addScore = function(){
+var total=0;
+for(var i in this.turn) {
+  total += this.turn[i];
+ }
+ this.score.push(total)
+ console.log(this.score)
 }
-
-
-  // function Player(score) {
-  //   this.score = 0
-  // }
-  //
-  // fu
-
-  // Player.prototype.addTurn = function(turn) {
-  //   if ()
-  //   //sum = add dices together
-  //
-  //
-  //
-
-
-
-
-
-  function Game(player1, player2, score1, score2) {
-    this.player1 = player1;
-    this.player1 = player1;
-    this.score1 = 0;
-    this.score2 = 0;
-  }
-  if $("#hold1").click(function(event) {
-      event.preventDefault();
-      
-  }
-  // flippy = new Thing('Flippy', 44, "5 foot 2")
-  //
-  // {
-  //   name: 'Flippy',
-  //   age: 44,
-  //   object: {
-  //     height: "5 foot 2"
-  //   }
-  // }
-  //
-  // flippy.object.height
-  // flippy[object][height]
-  //
-
-
-
-
-
 
 
 //user interface
 $(document).ready(function(){
-  var pigDice = new Game();
-  console.log(pigDice)
-  // call your game constructor here to make a new game
   var turn = [];
+  var player1 = new Player ()
+  var player2 = new Player ()
+  $("#hold1").click(function(event) {
+    event.preventDefault();
+    player1.addScore(player1.turn)
+    console.log(player1.score)
+  })
+  $("#hold2").click(function(event) {
+    event.preventDefault();
+    player2.addScore(player2.turn)
+    console.log(player2.score)
+  })
+
   $("#button").click(function(event){
     event.preventDefault()
     var dice = Math.floor((Math.random() * 6) + 1)
-    turn.push(dice);
+    player1.turn.push(dice)
+    player2.turn.push(dice)
+    console.log(player1.turn)
     $(".results").append(dice + ", ");
-    var result = pigDice(turn)
-    console.log(result)
   })
   })

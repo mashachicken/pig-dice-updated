0ddb6a309383b06f7b50eae6e20507b65b723715
Student <example@example.com>
2019-11-07 16:02:15 -0800

    add hold button functions and branching. it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-//
 // Game.prototype.addScores = function(turn) {
+//
 //   for (var i = 1; i < turn.length; i++){
 //     if (turn.includes(1)){
 //       alert("Next player's turn")
@@ -40,25 +40,42 @@ $(document).ready(function(){
   var turn = [];
   var player1 = new Player ()
   var player2 = new Player ()
+
   $("#hold1").click(function(event) {
     event.preventDefault();
-    player1.addScore(player1.turn)
-    console.log(player1.score)
+    player1.addScore(player1.turn);
     $("#score1").text(player1.score);
-  })
+  //   if (player1.turn.includes(2) || player1.turn.includes(3) || player1.turn.includes(4) || player1.turn.includes(5) || player1.turn.includes(6) ) {
+  //   player1.addScore(player1.turn);
+  //   console.log(player1.turn)
+  // }
+})
   $("#hold2").click(function(event) {
     event.preventDefault();
-    player2.addScore(player2.turn)
-    console.log(player2.score)
+    player2.addScore(player2.turn);
     $("#score2").text(player2.score);
   })
+
 
   $("#button").click(function(event){
     event.preventDefault()
     var dice = Math.floor((Math.random() * 6) + 1)
-    player1.turn.push(dice)
-    player2.turn.push(dice)
-    console.log(player1.turn)
     $(".results").append(dice + ", ");
-  })
-  })
+    // player2.turn.push(dice)
+    console.log(player1.turn)
+    if (dice !== 1){
+      player1.turn.push(dice)
+      player2.turn.push(dice)
+    }
+    else {
+      player1.turn = 0;
+      alert("1! Next Player's turn!")
+     }
+//     else if (player1.turn.includes(2) || player1.turn.includes(3) || player1.turn.includes(4) || player1.turn.includes(5) || player1.turn.includes(6) ) {
+//     player1.addScore(player1.turn);
+//     console.log(player1.turn)
+//     $("#score1").text(player1.score);
+// }
+
+})
+})
